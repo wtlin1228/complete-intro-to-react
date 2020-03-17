f77c57c1ca879ad7ad39d636911a327b35ef31b5
wtlin1228 <wtlin1228@gmail.com>
2020-03-17 22:24:34 +0800

    jsx composite components & expressions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import Pet from "./Pet";
 
-const Pet = ({ name = "Leo", animal = "Sheep", breed = "Fuck" } = {}) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
-
-// your code is going to go here. for now
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" bread="Havanese" />
+      <Pet name="Pepper" animal="Bird" bread="Cockatiel" />
+      <Pet name="Doink" animal="Cat" bread="Mixed" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function Pet({
+  name = "Leo",
+  animal = "Sheep",
+  breed = "Fuck"
+} = {}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+}
