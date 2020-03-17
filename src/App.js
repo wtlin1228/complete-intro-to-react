e47c9134632fad98fed544e3bec984bb05c64a3d
wtlin1228 <wtlin1228@gmail.com>
2020-03-17 13:26:08 +0800

    setup tools: prettier, eslint, parcel

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "react-dom";
+
+const Pet = ({ name = "Leo", animal = "Sheep", breed = "Fuck" } = {}) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+// your code is going to go here. for now
+const App = () => {
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
+  ]);
+};
+
+render(React.createElement(App), document.getElementById("root"));
