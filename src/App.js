4e6527bd36328ff45aee36d2b66b2f9e0b184da4
wtlin1228 <wtlin1228@gmail.com>
2020-03-24 16:50:47 +0800

    add emotion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -11,9 +12,7 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
+          <NavBar />
           <Router>
             <SearchParams path="/" />
             <Details path="/details/:id" />
